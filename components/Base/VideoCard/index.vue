944069eeb6c0e93745902aa0c33c20dd9eff91ee
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-01-06 15:58:45 +0700

    fix: fix height of card sections

--- components/Base/VideoCard/index.vue
+++ components/Base/VideoCard/index.vue
@@ -6,9 +6,9 @@
   >
     <!-- Video Section-->
     <img src="@/assets/images/avengers.webp" alt="Video Cover" class="group-hover:hidden">
-    <iframe :src="src" frameborder="0" allowfullscreen class="hidden group-hover:block h-[123px] rounded-[0.2vw]"></iframe>
+    <iframe :src="src" frameborder="0" allowfullscreen class="hidden group-hover:block h-1/2 rounded-[0.2vw] w-full"></iframe>
     <!-- Button Section -->
-    <div class="scale-y-0 opacity-0 h-0 group-hover:h-[123px] group-hover:scale-y-100 group-hover:opacity-100 bg-[#111111] transition-opacity duration-300 delay-200 ease-in-out flex flex-col p-4 group-hover:shadow-xl">
+    <div class="scale-y-0 opacity-0 h-0 group-hover:h-1/2 group-hover:scale-y-100 group-hover:opacity-100 bg-[#111111] transition-opacity duration-300 delay-200 ease-in-out flex flex-col p-4 group-hover:shadow-xl">
       <!-- Action Button -->
       <div class="flex justify-between">
         <div class="flex items-start">
