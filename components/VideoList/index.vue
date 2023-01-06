32d692a452ad6465c9770c6f4b5f43121fcee295
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-01-06 08:03:11 +0700

    feat: add explore button

--- components/VideoList/index.vue
+++ components/VideoList/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="ml-[4%] max-h-[200px]">
-    <h1 class="font-netflixsans text-[#e5e5e5] leading-[1.25vw] font-medium text-[1.4vw] mb-5 hover:cursor-pointer">
+    <h1 class="flex font-netflixsans text-[#e5e5e5] hover:text-white leading-[1.25vw] font-medium text-[1.4vw] mb-5 hover:cursor-pointer items-end group">
       {{ title }}
+      <button class="text-[#54b9c5] text-[.9vw] leading-[.8vw] font-bold ml-3 trans-explore opacity-0 group-hover:opacity-100 w-auto text-start">
+        Explore All >
+      </button>
     </h1>
     <swiper ref="mySwiper">
       <swiper-slide><BaseVideoCard /></swiper-slide>
@@ -33,5 +36,8 @@ export default {
 <style lang="scss" scoped>
 .swiper-slide {
   @apply max-w-[15vw];
+}
+.trans-explore {
+  transition: opacity 1s, transform .75s;
 }
 </style>
