10c3c40d13d4cff3e4c56423232ccfda662da4b5
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-01-16 12:57:54 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,46 @@
     <VideoList title="Trending Now" class="mt-[2%] 2xl:mt-[4%]" />
     <VideoList title="Recently Added" class="mt-[2%] 2xl:mt-[4%]" />
     <VideoList title="Marvel Studios" class="mt-[2%] 2xl:mt-[4%]" />
+    <!-- <div class="grid grid-cols-6 gap-3">
+      <div v-for="i in 18" :key="i" class="group hover:scale-[1.8] h-[200px] hover:delay-1000 transition">
+        <div class="w-[200px] h-full bg-red-400 group-hover:delay-1000 group-hover:h-0">
+        </div>
+        <div class="w-[200px] group-hover:h-1/2 bg-blue-400 group-hover:delay-1000 h-0">
+        </div>
+        <div class="w-[200px] h-0 group-hover:h-1/2 bg-green-400 group-hover:delay-1000">
+        </div>
+      </div>
+    </div> -->
+    <div class="grid grid-flow-col gap-3">
+      <div v-for="i in 18" :key="i" class="group hover:scale-[1.8] h-[200px] hover:delay-1000 transition">
+        <div class="w-[200px] h-full bg-red-400 group-hover:delay-1000 group-hover:h-0">
+        </div>
+        <div class="w-[200px] group-hover:h-1/2 bg-blue-400 group-hover:delay-1000 h-0">
+        </div>
+        <div class="w-[200px] h-0 group-hover:h-1/2 bg-green-400 group-hover:delay-1000">
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-flow-col gap-3 mt-[2%] 2xl:mt-[4%]">
+      <div v-for="i in 18" :key="i" class="group hover:scale-[1.8] h-[200px] hover:delay-1000 transition">
+        <div class="w-[200px] h-full bg-red-400 group-hover:delay-1000 group-hover:h-0">
+        </div>
+        <div class="w-[200px] group-hover:h-1/2 bg-blue-400 group-hover:delay-1000 h-0">
+        </div>
+        <div class="w-[200px] h-0 group-hover:h-1/2 bg-green-400 group-hover:delay-1000">
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-flow-col gap-3 mt-[2%] 2xl:mt-[4%]">
+      <div v-for="i in 18" :key="i" class="group hover:scale-[1.8] h-[200px] hover:delay-1000 transition">
+        <div class="w-[200px] h-full bg-red-400 group-hover:delay-1000 group-hover:h-0">
+        </div>
+        <div class="w-[200px] group-hover:h-1/2 bg-blue-400 group-hover:delay-1000 h-0">
+        </div>
+        <div class="w-[200px] h-0 group-hover:h-1/2 bg-green-400 group-hover:delay-1000">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
