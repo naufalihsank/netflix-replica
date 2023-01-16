e93bb98f8c157e1b8a9d3c5ab180b111e303c0bc
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2023-01-16 12:47:00 +0700

    fix: fix margin between video list

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-white pb-[50px]">
     <HomePlayer />
-    <VideoList title="Trending Now" class="mt-[4%]" />
-    <VideoList title="Recently Added" class="mt-[4%]" />
-    <VideoList title="Marvel Studios" class="mt-[4%]" />
+    <VideoList title="Trending Now" class="mt-[2%] 2xl:mt-[4%]" />
+    <VideoList title="Recently Added" class="mt-[2%] 2xl:mt-[4%]" />
+    <VideoList title="Marvel Studios" class="mt-[2%] 2xl:mt-[4%]" />
   </div>
 </template>
 <script>
